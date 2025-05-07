d2ca726b63e42a764be5d1590b6d81612bd093c4
Pavel Durov <pavel@p3ld3v.dev>
2025-05-07 20:46:09 +0100

    Parse operands for stackmaps and patchpoints.

--- llvm/lib/CodeGen/StackMaps.cpp
+++ llvm/lib/CodeGen/StackMaps.cpp
@@ -329,8 +329,13 @@ MachineInstr::const_mop_iterator StackMaps::parseOperand(
           if (TReg == DwarfRegNum) {
             continue;
           }
+          // dbgs() << "Patchpoint - erasing TReg: " << TReg << "\n";
           Extras.erase(TReg);
           for (auto X : SpillOffsets[TReg]) {
+            // if (InstrMI->getOpcode() == TargetOpcode::PATCHPOINT) {
+            //   dbgs() << "Patchpoint " << InstrMI->getOperand(0).getImm() << "
+            //   - erasing TReg: " << TReg << " with SpillOffset: "<< X << "\n";
+            // }
             Extras.erase(X);
           }
         }
@@ -597,38 +602,20 @@ void StackMaps::recordStackMapOpers(
   std::set<int64_t> TrackedRegisters;
 
   if (MI.getOpcode() == TargetOpcode::PATCHPOINT) {
-    // Example PATCHPOINT structure:
-    //  PATCHPOINT 0, 13, @__ykrt_control_point, 3, 0, $rdi, $rsi, $rdx, 0,
-    //  $rbp, -48, ...
-    // PATCHPOINT operands (by index):
-    // [0]  Patchpoint ID (e.g., 0)
-    // [1]  Patchpoint size (e.g., 13)
-    // [2]  Function name to call (e.g., @__ykrt_control_point)
-    // [3]  Number of arguments (e.g., 3)
-    // [4]  First argument register (e.g., $rdi)
-    // [5]  Second argument register (e.g., $rsi)
-    // [6]  Third argument register (e.g., $rdx)
-    // [7]  Zero or padding
-    // [8]  Frame pointer register (e.g., $rbp)
-    // [9]  Stack offset (relative to frame pointer, e.g., -48)
-    // ...
-    auto firstArgIdx = 4;
-    auto numArgs = MI.getOperand(3).getImm();
-    auto paddingOperandCount = 2;
-    auto argLength = numArgs + firstArgIdx + paddingOperandCount;
-    for (const auto *Op = MI.operands_begin(); Op != MI.operands_end(); Op++) {
-      if (argLength >= 0) {
-        argLength--;
-        continue;
-      }
-      if (Op->isReg() && !Op->isImplicit() && !Op->isUndef()) {
-        TrackedRegisters.insert(getDwarfRegNum(Op->getReg(), TRI));
+    PatchPointOpers Opers(&MI);
+    for (unsigned I = Opers.getStackMapStartIdx(); I < MI.getNumOperands();
+         I++) {
+      const MachineOperand &Op = MI.getOperand(I);
+      if (Op.isReg() && !Op.isImplicit() && !Op.isUndef()) {
+        TrackedRegisters.insert(getDwarfRegNum(Op.getReg(), TRI));
       }
     }
   } else {
-    for (const auto *Op = MI.operands_begin(); Op != MI.operands_end(); Op++) {
-      if (Op->isReg() && !Op->isImplicit() && !Op->isUndef()) {
-        TrackedRegisters.insert(getDwarfRegNum(Op->getReg(), TRI));
+    StackMapOpers Opers(&MI);
+    for (unsigned I = Opers.getVarIdx(); I < MI.getNumOperands(); I++) {
+      const MachineOperand &Op = MI.getOperand(I);
+      if (Op.isReg() && !Op.isImplicit() && !Op.isUndef()) {
+        TrackedRegisters.insert(getDwarfRegNum(Op.getReg(), TRI));
       }
     }
   }
